d6f7f51dfbd42a3742d13e95f515c5bf196795c8
Gsv <Gusevpvl@gmail.com>
2019-01-18 23:54:38 +0300

    DB OK

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,38 +2,49 @@ package com.company;
 
 import com.company.neuralnet.*;
 import java.sql.*;
+import java.*;
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 import java.io.File;
+
 //Comment from home
 
 public class Main {
-
     public static void main(String[] args) {
 
-        /*try {
-            DriverManager.registerDriver((Driver) Class.forName("com.mysql.jdbc.Driver").newInstance());
+        Connection conn = null;
+        String dbName = "NNDB";
+        String serverip="192.168.2.227";
+        String serverport="3306";
+        String url = "jdbc:sqlserver://"+serverip+"\\Student"+";databaseName="+dbName+"";
+        Statement stmt = null;
+        ResultSet result = null;
+        String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        String databaseUserName = "Student";
+        String databasePassword = "S123456s";
+        try {
 
-            StringBuilder url = new StringBuilder();
+            Class.forName(driver).newInstance();
+            conn = DriverManager.getConnection(url, databaseUserName, databasePassword);
+            stmt = conn.createStatement();
+            result = null;
+            String pa,us;
+            result = stmt.executeQuery("select * from ExpirementInputs ");
 
-            url.
-                    append("jdbc:mysql://").   //db type
-                    append("localhost:").      //host name
-                    append("3306/").           //port
-                    append("world").           //db name
-                    append("user=root&").      //login
-                    append("password=root");   //password
+            while (result.next()) {
+                us=result.getString("det1");
+                System.out.println(us);
+            }
 
-            System.out.println("URL: " + url + "\n");
+            conn.close();
+            System.out.println("Connection OK");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-            Connection connection = DriverManager.getConnection(url.toString(), "root", "root");
 
-        } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-            System.out.println("Error");
-        }*/
-
-        NeuralNet newnet=new NeuralNet();
+        /*NeuralNet newnet=new NeuralNet();
         NeuralNet.Train(newnet);
-        NeuralNet.Test(newnet);
+        NeuralNet.Test(newnet);*/
 
 
 
