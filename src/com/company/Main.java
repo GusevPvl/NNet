37442282ff97d291ff6539f6cdcab2446d82a6b5
Gsv <Gusevpvl@gmail.com>
2019-01-21 10:28:42 +0300

    Jar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,9 +15,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
-
-
+        //Запуск сети
         NeuralNet.ResetWeights();
         NeuralNet newnet=new NeuralNet();
         NeuralNet.Train(newnet);
